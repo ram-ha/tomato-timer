eda41c2a60da5cb19462d9f841e9a5c3e793702a
ram-ha <dlgkfka2616@gmail.com>
2023-10-02 01:09:19 +0900

    :zap: recoil-persist 추가

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,24 +1,34 @@
 import { atom, selector } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom: timerPersist } = recoilPersist({
+    key: 'timerLocalStorage',
+    storage: localStorage,
+});
 
 export const timerState = atom<number>({
     key: 'timerState',
     default: 1500,
-})
+    effects_UNSTABLE: [timerPersist],
+});
+
 export const roundsState = atom({
     key: 'roundsState',
     default: 0,
 });
+
 export const goalsState = atom({
     key: 'goalsState',
     default: 0,
 });
+
 export const isStartState = atom({
-    key: "isStartState",
+    key: 'isStartState',
     default: false,
-})
+});
 
 export const timeSelector = selector({
-    key: "timeSelector",
+    key: 'timeSelector',
     get: ({ get }) => {
         const time = get(timerState);
         const minute = String(Math.floor(time / 60)).padStart(2, "0");
@@ -28,4 +38,4 @@ export const timeSelector = selector({
             seconds,
         };
     }
-})
+});
